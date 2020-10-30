0a5d7a652f80ab6cc36fb93d82bb1dc7fe9b90e4
Kio <48633146+Kiomars93@users.noreply.github.com>
2020-10-30 17:17:02 +0100

    Update my current products into a 2 x 2 grid
    I will restructure the code later.

--- WPFStore/WPFStore/MainWindow.xaml.cs
+++ WPFStore/WPFStore/MainWindow.xaml.cs
@@ -55,47 +55,177 @@ namespace WPFStore
             grid.Margin = new Thickness(5);
             grid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
             grid.ColumnDefinitions.Add(new ColumnDefinition());
-            var currentProductGrid = new StackPanel
-            {
-                Orientation = Orientation.Vertical
-            };
+            Grid currentProductGrid = new Grid();
             grid.Children.Add(currentProductGrid);
+            currentProductGrid.RowDefinitions.Add(new RowDefinition());
+            currentProductGrid.RowDefinitions.Add(new RowDefinition());
+            currentProductGrid.RowDefinitions.Add(new RowDefinition());
+            currentProductGrid.RowDefinitions.Add(new RowDefinition());
+            currentProductGrid.RowDefinitions.Add(new RowDefinition());
+            currentProductGrid.RowDefinitions.Add(new RowDefinition());
+            currentProductGrid.RowDefinitions.Add(new RowDefinition());
+            currentProductGrid.RowDefinitions.Add(new RowDefinition());
+            currentProductGrid.RowDefinitions.Add(new RowDefinition());
+            currentProductGrid.RowDefinitions.Add(new RowDefinition());
+            currentProductGrid.RowDefinitions.Add(new RowDefinition());
+            currentProductGrid.RowDefinitions.Add(new RowDefinition());
+            currentProductGrid.RowDefinitions.Add(new RowDefinition());
+            currentProductGrid.RowDefinitions.Add(new RowDefinition());
+            currentProductGrid.RowDefinitions.Add(new RowDefinition());
+            currentProductGrid.RowDefinitions.Add(new RowDefinition());
+            currentProductGrid.RowDefinitions.Add(new RowDefinition());
+            currentProductGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            currentProductGrid.ColumnDefinitions.Add(new ColumnDefinition());
+
+
+
             var currentProductLabel = new Label
             {
                 Content = "Welcome to my shop. This is our current products:",
                 HorizontalAlignment = HorizontalAlignment.Center,
                 VerticalAlignment = VerticalAlignment.Center,
-                FontSize = 14
+                FontSize = 14,
+                Margin = new Thickness(5)
             };
             currentProductGrid.Children.Add(currentProductLabel);
             Grid.SetRow(currentProductLabel, 0);
+            Grid.SetColumnSpan(currentProductLabel, 2);
 
-            foreach (var productItem in productList)
+            var firstProduct = productList[0];
+            var firstTitleLabel = new Label
             {
-                var titleLabel = new Label
-                {
-                    Content = productItem.Title,
-                };
+                Content = firstProduct.Title,
+            };
 
-                var descriptionLabel = new Label
-                {
-                    Content = productItem.Description
-                };
+            var firstDescriptionLabel = new Label
+            {
+                Content = firstProduct.Description
+            };
 
-                var priceLabel = new Label
-                {
-                    Content = productItem.Price
-                };
+            var firstPriceLabel = new Label
+            {
+                Content = firstProduct.Price
+            };
 
-                var imageLabel = new Label
-                {
-                    Content = CreateImage(productItem.Image)
-                };
-                currentProductGrid.Children.Add(titleLabel);
-                currentProductGrid.Children.Add(descriptionLabel);
-                currentProductGrid.Children.Add(priceLabel);
-                currentProductGrid.Children.Add(imageLabel);
-            }
+            var firstImageLabel = new Label
+            {
+                Content = CreateImage(firstProduct.Image)
+            };
+            currentProductGrid.Children.Add(firstTitleLabel);
+            currentProductGrid.Children.Add(firstDescriptionLabel);
+            currentProductGrid.Children.Add(firstPriceLabel);
+            currentProductGrid.Children.Add(firstImageLabel);
+            Grid.SetRow(firstTitleLabel, 1);
+            Grid.SetColumn(firstTitleLabel, 0);
+            Grid.SetRow(firstDescriptionLabel, 2);
+            Grid.SetColumn(firstDescriptionLabel, 0);
+            Grid.SetRow(firstPriceLabel, 3);
+            Grid.SetColumn(firstPriceLabel, 0);
+            Grid.SetRow(firstImageLabel, 4);
+            Grid.SetColumn(firstImageLabel, 0);
+            
+            var secondProduct = productList[1];
+
+            var secondTitleLabel = new Label
+            {
+                Content = secondProduct.Title,
+            };
+
+            var secondDescriptionLabel = new Label
+            {
+                Content = secondProduct.Description
+            };
+
+            var secondPriceLabel = new Label
+            {
+                Content = secondProduct.Price
+            };
+
+            var secondImageLabel = new Label
+            {
+                Content = CreateImage(secondProduct.Image)
+            };
+            currentProductGrid.Children.Add(secondTitleLabel);
+            currentProductGrid.Children.Add(secondDescriptionLabel);
+            currentProductGrid.Children.Add(secondPriceLabel);
+            currentProductGrid.Children.Add(secondImageLabel);
+            Grid.SetRow(secondTitleLabel, 1);
+            Grid.SetRow(secondDescriptionLabel, 2);
+            Grid.SetRow(secondPriceLabel, 3);
+            Grid.SetRow(secondImageLabel, 4);
+            Grid.SetColumn(secondTitleLabel, 1);
+            Grid.SetColumn(secondDescriptionLabel, 1);
+            Grid.SetColumn(secondPriceLabel, 1);
+            Grid.SetColumn(secondImageLabel, 1);
+
+            var thirdProduct = productList[2];
+
+            var thirdTitleLabel = new Label
+            {
+                Content = thirdProduct.Title,
+            };
+
+            var thirdDescriptionLabel = new Label
+            {
+                Content = thirdProduct.Description
+            };
+
+            var thirdPriceLabel = new Label
+            {
+                Content = thirdProduct.Price
+            };
+
+            var thirdImageLabel = new Label
+            {
+                Content = CreateImage(thirdProduct.Image)
+            };
+            currentProductGrid.Children.Add(thirdTitleLabel);
+            currentProductGrid.Children.Add(thirdDescriptionLabel);
+            currentProductGrid.Children.Add(thirdPriceLabel);
+            currentProductGrid.Children.Add(thirdImageLabel);
+            Grid.SetRow(thirdTitleLabel, 5);
+            Grid.SetRow(thirdDescriptionLabel, 6);
+            Grid.SetRow(thirdPriceLabel, 7);
+            Grid.SetRow(thirdImageLabel, 8);
+            Grid.SetColumn(thirdTitleLabel, 0);
+            Grid.SetColumn(thirdDescriptionLabel, 0);
+            Grid.SetColumn(thirdPriceLabel, 0);
+            Grid.SetColumn(thirdImageLabel, 0);
+
+
+            var foruthProduct = productList[3];
+
+            var fourthTitleLabel = new Label
+            {
+                Content = foruthProduct.Title,
+            };
+
+            var fourthDescriptionLabel = new Label
+            {
+                Content = foruthProduct.Description
+            };
+
+            var fourthPriceLabel = new Label
+            {
+                Content = foruthProduct.Price
+            };
+
+            var fourthImageLabel = new Label
+            {
+                Content = CreateImage(foruthProduct.Image)
+            };
+            currentProductGrid.Children.Add(fourthTitleLabel);
+            currentProductGrid.Children.Add(fourthDescriptionLabel);
+            currentProductGrid.Children.Add(fourthPriceLabel);
+            currentProductGrid.Children.Add(fourthImageLabel);
+            Grid.SetRow(fourthTitleLabel, 5);
+            Grid.SetRow(fourthDescriptionLabel, 6);
+            Grid.SetRow(fourthPriceLabel, 7);
+            Grid.SetRow(fourthImageLabel, 8);
+            Grid.SetColumn(fourthTitleLabel, 1);
+            Grid.SetColumn(fourthDescriptionLabel, 1);
+            Grid.SetColumn(fourthPriceLabel, 1);
+            Grid.SetColumn(fourthImageLabel, 1);
 
 
 
